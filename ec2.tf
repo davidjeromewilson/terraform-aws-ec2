8844a7f03a97f860c47f28c616b300f23be77dea
davidjeromewilson <david.jerome.wilson@gmail.com>
2024-05-15 18:44:03 +0000

    Add variable to choose if ip is public

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "education_bastion" {
   ami                         = data.aws_ami.ubuntu20.id
   subnet_id                   = module.vpc.public_subnets[0]
   key_name                    = aws_key_pair.deployer.key_name
-  associate_public_ip_address = false
+  associate_public_ip_address = var.public_ip
   instance_type               = var.instance_type
   tags = {
     Name = "${random_pet.random.id}-instance"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "instance_type" {
   description = "AWS EC2 instance type"
 }
+
+variable "public_ip" {
+    description = "Do you need a public facing ip?"
+}
